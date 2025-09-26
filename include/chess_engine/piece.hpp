1133a2f96c029a266968b58376c3b46a54a77365
Baptiste Penot <baptiste.penot@gmail.com>
2025-09-26 20:09:20 +0200

    making switch case  prettier

--- include/chess_engine/piece.hpp
+++ include/chess_engine/piece.hpp
@@ -32,46 +32,20 @@ class Piece {
    */
   constexpr Piece(char c = '.') {
     m_symbol = c;
-    switch (c) {
-      case 'P':
-        m_type = P;
-        break;
-      case 'N':
-        m_type = N;
-        break;
-      case 'B':
-        m_type = B;
-        break;
-      case 'R':
-        m_type = R;
-        break;
-      case 'Q':
-        m_type = Q;
-        break;
-      case 'K':
-        m_type = K;
-        break;
-      case 'p':
-        m_type = p;
-        break;
-      case 'n':
-        m_type = n;
-        break;
-      case 'b':
-        m_type = b;
-        break;
-      case 'r':
-        m_type = r;
-        break;
-      case 'q':
-        m_type = q;
-        break;
-      case 'k':
-        m_type = k;
-        break;
-      case '.':
-        m_type = NO_PIECE;
-        break;
+    switch (c) {  // clang-format off
+      case 'P': m_type = P; break;
+      case 'N': m_type = N; break;
+      case 'B': m_type = B; break;
+      case 'R': m_type = R; break;
+      case 'Q': m_type = Q; break;
+      case 'K': m_type = K; break;
+      case 'p': m_type = p; break;
+      case 'n': m_type = n; break;
+      case 'b': m_type = b; break;
+      case 'r': m_type = r; break;
+      case 'q': m_type = q; break;
+      case 'k': m_type = k; break;
+      case '.': m_type = NO_PIECE; break;  // clang-format on
       default:
         const std::string msg = fmt::format("Invalid piece character {}", c);
         throw std::invalid_argument(msg);
