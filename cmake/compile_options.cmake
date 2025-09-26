9bd0bcc5cfe3bc878c1555b5428750dd61dc86b8
Baptiste Penot <baptiste.penot@gmail.com>
2025-09-26 20:05:44 +0200

    commented with the issue to log

--- cmake/compile_options.cmake
+++ cmake/compile_options.cmake
@@ -1,5 +1,8 @@
 
-# constexpr usage with fmt requires the use of /utf-8 compile option on MSVC
 if (MSVC)
+    # constexpr usage with fmt requires the use of /utf-8 compile option on MSVC
+    # [build] vcpkg_installed\x64-windows\include\fmt\base.h(458,28):
+    #   error C2338: static_assert failed:
+    #       'Unicode support requires compiling with /utf-8'
     add_compile_options(/utf-8)
 endif()
